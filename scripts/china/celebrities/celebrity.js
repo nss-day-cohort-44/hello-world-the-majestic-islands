aaf3f9f41cb026891454be85d9c13fd564acd26e
admin <christopherleorondeau@gmail.com>
2020-10-13 16:17:36 -0500

    added citizen function script

---
+++ scripts/china/celebrities/celebrity.js
@@ -0,0 +1,5 @@
+export const celebrity = (celebObj) =>{
+    return `
+    <div> 
+    <p>${celebObj.person}</p>`
+}
--- scripts/china/celebrities/celebrityDataProvider.js
+++ scripts/china/celebrities/celebrityDataProvider.js
@@ -1,4 +1,13 @@
 const citizens = [
+
     { person: "Confucius" },
-    
+    { person: "Tu Youyou" },
+    { person: "Dalia Lama" },
+    { person: "Yao Ming" },
+    { person: "Mao Zedong" }
+       
 ]
+
+export const useCitizen = () => {
+    return [...citizens]
+}
