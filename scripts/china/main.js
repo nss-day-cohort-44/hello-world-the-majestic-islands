0afa7fda9932c9d0f0ba9550fa41bc3bafad42d3
admin <christopherleorondeau@gmail.com>
2020-10-13 15:45:03 -0500

    completed landmark module

--- scripts/china/main.js
+++ scripts/china/main.js
@@ -1,8 +1,9 @@
 console.log("Welcome to the main module")
 import { CityList } from "./cities/cityList.js"
+import { LandmarkList } from "./landmarks/landmarkList.js"
 
 CityList()
-  
+LandmarkList()  
 
 
 
